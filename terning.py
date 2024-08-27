a7b2021de5ec9f9638d2cfaa0df20a61676a7320
Kristian Lien <krnlien@gmail.com>
2024-08-27 17:23:00 +0200

    added GUI

--- terning.py
+++ terning.py
@@ -3,8 +3,51 @@ import os
 import random
 
 fil = "terningdat.txt"
-
 terningdb = []
+
+gui = True
+
+def terning1():
+    print("+-------+")
+    print("|       |")
+    print("|   O   |")
+    print("|       |")
+    print("+-------+")
+
+def terning2():
+    print("+-------+")
+    print("| O     |")
+    print("|       |")
+    print("|     O |")
+    print("+-------+")
+
+def terning3():
+    print("+-------+")
+    print("| O     |")
+    print("|   O   |")
+    print("|     O |")
+    print("+-------+")
+
+def terning4():
+    print("+-------+")
+    print("| O   O |")
+    print("|       |")
+    print("| O   O |")
+    print("+-------+")
+
+def terning5():
+    print("+-------+")
+    print("| O   O |")
+    print("|   O   |")
+    print("| O   O |")
+    print("+-------+")
+
+def terning6():
+    print("+-------+")
+    print("| O   O |")
+    print("| O   O |")
+    print("| O   O |")
+    print("+-------+")
 
 def console_clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -30,7 +73,21 @@ time.sleep(0.5)
 console_clear()
 for i in range(antall_terninger):
     var = random.randint(1, 6)
-    print("Terning", i+1, ":", var)
+    if gui == True:
+        if var == 1:
+            terning1()
+        elif var == 2:
+            terning2()
+        elif var == 3:
+            terning3()
+        elif var == 4:
+            terning4()
+        elif var == 5:
+            terning5()
+        elif var == 6:
+            terning6()
+    elif gui == False:
+        print("Terning", i+1, ":", var)
     entry_string = "Terning " + str(i+1) + ":"
     entry = [entry_string, var]
     #this is a really bad way of doing this, will optimize
